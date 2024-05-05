0c7fa7a466601baff04701ebf32e15b554160f67
rmheuer <63077980+rmheuer@users.noreply.github.com>
2024-05-05 10:24:48 -0500

    [Robot] Remove empty GeometryUtil

--- Robot/src/main/java/com/swrobotics/robot/utils/GeometryUtil.java
+++
@@ -1,8 +0,0 @@
-package com.swrobotics.robot.utils;
-
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Twist2d;
-
-public final class GeometryUtil {
-}
