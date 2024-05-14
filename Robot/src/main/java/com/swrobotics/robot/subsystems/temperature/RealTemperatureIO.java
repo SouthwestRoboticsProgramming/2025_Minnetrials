7574caf8a048f2c0dd050508758c1dbaac390398
rmheuer <63077980+rmheuer@users.noreply.github.com>
2024-05-14 16:39:59 -0500

    [Robot] Only do motor grouping once

--- Robot/src/main/java/com/swrobotics/robot/subsystems/temperature/RealTemperatureIO.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/temperature/RealTemperatureIO.java
@@ -13,41 +13,36 @@ public final class RealTemperatureIO implements TemperatureIO {
     private record TrackedMotor(String name, String canBus, StatusSignal<Double> tempStatus) {}
 
     private final List<TrackedMotor> motors = new ArrayList<>();
-    private BaseStatusSignal[] allSignals;
+    private BaseStatusSignal[][] allSignals = null;
 
     @Override
     public void updateInputs(Inputs inputs) {
-        Map<String, List<BaseStatusSignal>> groups = new HashMap<>();
-        for (TrackedMotor motor : motors) {
-            List<BaseStatusSignal> group = groups.computeIfAbsent(motor.canBus, (b) -> new ArrayList<>());
-            group.add(motor.tempStatus);
-        }
-
-        for (List<BaseStatusSignal> group : groups.values()) {
-            BaseStatusSignal[] signals = group.toArray(new BaseStatusSignal[0]);
-            BaseStatusSignal.refreshAll(signals);
-        }
-
         int count = motors.size();
-//
-//        // Only allocate arrays once to minimize performance impact
-//        if (allSignals == null) {
-//            allSignals = new BaseStatusSignal[count];
-//            for (int i = 0; i < count; i++)
-//                allSignals[i] = motors.get(i).tempStatus;
-//
-//            // Assume we'll always receive the same Inputs object, which should
-//            // always be the case
-//            inputs.names = new String[count];
-//            inputs.temperatures = new double[count];
-//        }
-//
-//        // Refresh all at once so it takes less time. We don't want to take
-//        // long here so we don't disturb the main robot loop timing
-//        BaseStatusSignal.refreshAll(allSignals);
-//
-        inputs.names = new String[count];
-        inputs.temperatures = new double[count];
+
+        // Only allocate arrays once
+        if (allSignals == null) {
+            Map<String, List<BaseStatusSignal>> groups = new HashMap<>();
+            for (TrackedMotor motor : motors) {
+                List<BaseStatusSignal> group = groups.computeIfAbsent(motor.canBus, (b) -> new ArrayList<>());
+                group.add(motor.tempStatus);
+            }
+
+            allSignals = new BaseStatusSignal[groups.size()][];
+            int groupIdx = 0;
+            for (List<BaseStatusSignal> group : groups.values()) {
+                BaseStatusSignal[] signals = group.toArray(new BaseStatusSignal[0]);
+                allSignals[groupIdx++] = signals;
+            }
+
+            inputs.names = new String[count];
+            inputs.temperatures = new double[count];
+        }
+
+        // Refresh all status signals grouped by CAN bus
+        for (BaseStatusSignal[] group : allSignals) {
+            BaseStatusSignal.refreshAll(group);
+        }
+
         for (int i = 0; i < count; i++) {
             TrackedMotor motor = motors.get(i);
             inputs.names[i] = motor.name();
